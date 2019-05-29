47355f9426727bfd4f99dcb6cd086a5414b8fa4b
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-05-29 14:16:33 +0100

    Create header and footer and style the page

---
+++ UI/js/main.js
@@ -0,0 +1,9 @@
+function loginPage() {
+  document.getElementById('main2').style.display = 'flex';
+  document.getElementById('main1').style.display = 'none';
+}
+
+function signupPage() {
+  document.getElementById('main1').style.display = 'flex';
+  document.getElementById('main2').style.display = 'none';
+}
