d975d5940f97c6375099a9c0763d5afdcf6cecb9
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-05-30 19:25:53 +0100

    Add functionality for creating candidates

--- UI/js/main.js
+++ UI/js/main.js
@@ -12,31 +12,32 @@ $(document).ready(function () {
 });
 
 //Register a user
-$('#userForm').submit(function (e) {
-  e.preventDefault();
-  const firstName = $('#fName').val();
-  const lastName = $('#lName').val();
-  const email = $('#eml').val();
-  const password = $('#pwd').val();
-  const url = $(this).attr('action');
-  const user = { firstName, lastName, email, password };
-  $.ajax({
-    url: url,
-    type: 'POST',
-    data: user,
-    success: function (data) {
-      $('.side2').html(`
+function addUser() {
+  $('#userForm').submit(function (e) {
+    e.preventDefault();
+    const firstName = $('#fName').val();
+    const lastName = $('#lName').val();
+    const email = $('#eml').val();
+    const password = $('#pwd').val();
+    const url = $(this).attr('action');
+    const user = { firstName, lastName, email, password };
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: user,
+      success: function (data) {
+        $('.side2').html(`
         <h2>You have successfully registered on VOVS</h2>
         <h3>Your details are </h3>
-        <b>Name: </b>${user.firstName} ${user.lastName} <br>
-        <b>Name: </b>${user.email} <br>
+        <b>Name: </b>${data.firstName} ${data.lastName} <br>
+        <b>Name: </b>${data.email} <br>
          `);
-      alert(`Congratulations ${user.firstName} ${user.lastName}, You have successfully registered on VOVS. You can now sign in and cast your vote`);
+        alert(`Congratulations ${user.firstName} ${user.lastName}, You have successfully registered on VOVS. You can now sign in and cast your vote`);
 
-    },
-    error: function (e) {
-      console.log(e.message);
-    }
+      },
+      error: function (e) {
+        console.log(e.message);
+      }
+    });
   });
-});
-
+}
